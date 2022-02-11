97148fde691260c097b56566a11937eaffc653a1
stutaylor2509 <57462368+stutaylor2509@users.noreply.github.com>
2022-02-11 14:21:19 +0000

    Homepage update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,23 @@
 import * as React from "react"
-import { Link } from "gatsby"
+//import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
-
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <Seo title="Hi-Risk Records | Hip Hop" />
+    <section class="hero">
     <StaticImage
-      src="../images/gatsby-astronaut.png"
+      src="../images/hr.png"
       width={300}
       quality={95}
       formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
+      alt="Hi Risk Records"
       style={{ marginBottom: `1.45rem` }}
     />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
+      </section>
+
   </Layout>
 )
 
